e9f5a22ee8d048ffcf5b651b3c8c95acd3b38bd3
taecjee <taecjee@naver.com>
2022-04-24 18:08:41 +0900

    III. 3. 연습문제 추가

--- III. 3. CNN.R
+++ III. 3. CNN.R
@@ -72,3 +72,93 @@ plot(history)
 evaluate(model, cifar$test$x, cifar$test$y, verbose = 0)
 
 evaluate(model, cifar$train$x, cifar$train$y, verbose = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################
+## 연습문제
+##########################################
+library(keras)
+
+# 데이터 준비
+fashion_mnist <- dataset_fashion_mnist()
+
+c(train_images, train_labels) %<-% fashion_mnist$train
+c(test_images, test_labels) %<-% fashion_mnist$test
+
+dim(train_images)
+dim(test_images)
+
+train_images2 <- array_reshape(train_images, dim = c(60000, 28, 28, 1))
+dim(train_images2)
+test_images2 <- array_reshape(test_images, dim = c(10000, 28, 28, 1))
+dim(test_images2)
+
+# Convolution Layer
+model <- keras_model_sequential() %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu", 
+                input_shape = c(28,28,1)) %>% 
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = "relu")
+
+summary(model)
+
+# Dense Layer
+model %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 64, activation = "relu") %>% 
+  layer_dense(units = 10, activation = "softmax")
+
+summary(model)
+
+# 모델 학습
+model %>% compile(
+  optimizer = "adam",
+  loss = "sparse_categorical_crossentropy",
+  metrics = "accuracy"
+)
+
+history <- model %>% 
+  fit(
+    x = train_images2, y = train_labels,
+    epochs = 10,
+    validation_split = 0.3,
+    verbose = 2
+  )
+
+# 모델 평가
+preds2 <- model %>% predict(test_images2) %>% k_argmax()
+
+preds2.array <- as.array(preds2)
+
+res2 <- data.frame(cbind(test_labels, preds2.array))
+
+table(res2)
+accuracy2 <- sum(res2$test_labels == res2$preds2.array) / nrow(res2)
+accuracy2
